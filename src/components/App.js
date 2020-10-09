62c0a9d9b384d7634d076a09ed89a57abfb715a7
gshnlgnv <gshnlgnv@gmail.com>
2020-10-10 00:09:45 +0300

    feat: cat arr

--- src/components/App.js
+++ src/components/App.js
@@ -20,8 +20,19 @@ const mouses = [
     "100 порций 5 мышей в подарок заказчик доволен",
 ];
 
-function App(){
-    return(
+function App() {
+    const catsNumber = 3;
+
+    const listCats = (catsNumber) => {
+        let arr = [];
+
+        for (let i = 0; i < catsNumber; i++) {
+            arr.push(<CatItem foodType={names[i]} foodWeight={weight[i]} mousesQnty={mouses[i]}/>);
+        }
+        return arr;
+    };
+
+    return (
         <div className="wrapper">
             <main>
                 <div className="main__wrapper">
@@ -29,9 +40,7 @@ function App(){
                         Ты сегодня покормил кота?
                     </div>
                     <div className="main__cats_types">
-                        <CatItem foodType={names[0]} foodWeight={weight[0]} mousesQnty={mouses[0]}/>
-                        <CatItem foodType={names[1]} foodWeight={weight[1]} mousesQnty={mouses[1]}/>
-                        <CatItem foodType={names[2]} foodWeight={weight[2]} mousesQnty={mouses[2]}/>
+                        {listCats(catsNumber)}
                     </div>
                 </div>
             </main>
